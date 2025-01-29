053e02b3ae105c0e973afa4f1db2ce57147f028b
BugHunter-X <askzenai@gmail.com>
2025-01-29 11:12:05 +0100

    Add bug.lua file

---
+++ bug.lua
@@ -0,0 +1,12 @@
+local function foo(t)
+  for k, v in pairs(t) do
+    if type(v) == "table" then
+      foo(v)
+    end
+  end
+end
+
+local t = {a = 1, b = {c = 2, d = {e = 3}}}
+foo(t)
+
+--This code will produce a stack overflow error if the table t is very deep.
